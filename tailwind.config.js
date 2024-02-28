f7ad2d2b280e2b4c3ee5df042687c0e2e5e219b7
Xela-Byte <xelaoladipupo@gmail.com>
2024-02-28 19:11:15 +0100

    New push

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { 
+      fontFamily: {
+      'body': ['"DM Sans"', 'san-serif'],
+    }},
   },
   plugins: [],
 }
